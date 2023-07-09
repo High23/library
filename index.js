e18a82c156f70f4edec418c753514d23931e4ca0
High23 <warenasb2@gmail.com>
2023-07-08 21:46:54 -0700

    Added js logic to show a book's information

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+let myLibrary = [];
+const libraryDiv = document.querySelector('.library');
 
+
+addBookToLibrary();
+
+myLibrary.forEach(book => {
+  const bookCard = document.createElement('div');
+  for (let key in book) {
+    const info = document.createElement('div');
+    info.textContent = book[key];
+    bookCard.appendChild(info);
+  }
+  libraryDiv.appendChild(bookCard);
+});
+ 
+
+
+
+
+function Book(title, author, numOfPages, read) {
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  const book = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
+  myLibrary.push(book);
+}
 
 
 
