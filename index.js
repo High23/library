00f4cdd827b2132665e314296870fdb0a8779406
High23 <warenasb2@gmail.com>
2023-07-10 02:58:02 -0700

    Added functionality to book card buttons

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
+// Global variables
+
 let myLibrary = [];
 const libraryDiv = document.querySelector('.library');
 const form = document.querySelector('form');
+const allInputs = document.querySelectorAll('input');
 const addBookButton = document.querySelector('.add-book');
+const addButton = document.querySelector('.add');
+const cancelButton = document.querySelector('.cancel');
+
+
+// Button event listeners
+
 addBookButton.addEventListener('click', () => {
-  form.classList.toggle('hidden');
+  form.classList.remove('hidden');
   addBookButton.disabled = true;
 });
 
-const inputs = document.querySelectorAll('input');
-
-
-const addButton = document.querySelector('.add')
 addButton.addEventListener('click', () => {
-  const title = inputs[0].value;
-  const author = inputs[1].value;
-  const pages = inputs[2].value;
-  const read = inputs[3].value;
-  if (title === '' || author === '' || pages === '' || read === ''){
+  const title = allInputs[0].value;
+  const author = allInputs[1].value;
+  const pages = allInputs[2].value;
+  let read = document.querySelector('[name=read]:checked');
+  read ? read = 'Read': read = 'Not read yet';
+  if (title === '' || author === '' || pages === ''){
     return;
   } else {
     addBookToLibrary(title, author, pages, read);
     displayBookInfo();
     form.classList.toggle('hidden');
     addBookButton.disabled = false;
-    clearInputs(inputs);
+    clearInputs(allInputs);
   }
 });
 
-const cancelButton = document.querySelector('.cancel');
 cancelButton.addEventListener('click', () => {
   form.classList.toggle('hidden');
   addBookButton.disabled = false;
 });
 
 
+// Functions
 
 function Book(title, author, numOfPages, read) {
   this.title = title;
@@ -49,24 +55,82 @@ function addBookToLibrary(title, author, numOfPages, read) {
 
 function displayBookInfo(){
   const bookCard = document.createElement('div');
-  for (let i = myLibrary.length - 1; i < myLibrary.length; i++){
+  let arrayLength = myLibrary.length;
+  for (let i = arrayLength - 1; i < arrayLength; i++) {
     for (let book in myLibrary[i]) {
       const info = document.createElement('div');
+      if (book === 'read') {
+        info.classList.add(`read-status`);
+        info.setAttribute('data-index-number', i);
+      }
       info.textContent = myLibrary[i][book];
       bookCard.appendChild(info);
     }
-  }
+    removeBtn(bookCard);
+    changeStatusBtn(bookCard, i);
+    bookCard.classList.add('book')
+    bookCard.setAttribute('data-index-number', i);
+  }  
   libraryDiv.appendChild(bookCard);
 }
 
-function clearInputs(inputs) {
-  inputs.forEach(input => {
+function clearInputs(allInputs) {
+  allInputs.forEach(input => {
     input.value = '';
-    input.addEventListener('submit', inputEnter);
+    input.addEventListener('submit', preventInputSubmit);
   });
 }
 
-function inputEnter(event) {
+function preventInputSubmit(event) {
   event.preventDefault();
 }
 
+function removeBtn(bookCard) {
+  // Creates the button and adds it to the DOM
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'Remove book'
+  removeButton.classList.add('remove');
+  bookCard.appendChild(removeButton);
+
+  // Removes a book from the DOM and the myLibrary array
+  removeButton.addEventListener('click', () => {
+    const bookIndex = bookCard.getAttribute('data-index-number');
+    myLibrary.splice(bookIndex, 1);
+    bookCard.remove();
+    updateDataIndexNumber();
+  });
+}
+
+function changeStatusBtn(bookCard) {
+  // Creates the button and adds it to the DOM
+  const statusButton = document.createElement('button');
+  statusButton.textContent = 'Change read status'
+  statusButton.classList.add('change-read-status');
+  bookCard.appendChild(statusButton);
+
+  // Changes a book's read status
+  statusButton.addEventListener('click', () => {
+    updateDataIndexNumber();
+    const bookIndex = bookCard.getAttribute('data-index-number');
+    const readStatus = document.querySelectorAll('.read-status');
+    if (myLibrary[bookIndex].read === 'Read') {
+      readStatus[bookIndex].textContent = 'Not read yet';
+      myLibrary[bookIndex].read = 'Not read yet';
+    } else {
+      readStatus[bookIndex].textContent = 'Read';
+      myLibrary[bookIndex].read = 'Read';
+    }
+  });
+}
+
+function updateDataIndexNumber() {
+  const books = document.querySelectorAll('.book');
+  books.forEach((book, value) => {
+    book.setAttribute('data-index-number', value);
+  })
+  
+  const status = document.querySelectorAll('.read-status');
+  status.forEach((status, value) => {
+    status.setAttribute('data-index-number', value);
+  })
+}
